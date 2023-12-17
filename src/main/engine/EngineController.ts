1788273a97938c4540a4aad202602f1be5b4c2fa
Atirudom <atirach.int@gmail.com>
2023-12-17 10:32:45 +0700

    develop engine

--- src/main/engine/EngineController.ts
+++ src/main/engine/EngineController.ts
@@ -1,8 +1,15 @@
 import Engine from './Engine';
 import IEngineController from './interfaces/IEngineController';
+import { SettingsModel } from './model/SettingsModel';
 
 export default class EngineController implements IEngineController {
   constructor(private engine: Engine) {}
+  getSettings(): SettingsModel {
+    throw new Error('Method not implemented.');
+  }
+  saveSettings(settings: SettingsModel): void {
+    throw new Error('Method not implemented.');
+  }
 
   connect(unitId: number): void {
     this.engine.plc.connect(unitId);
--- src/main/engine/interfaces/IEngineController.ts
+++ src/main/engine/interfaces/IEngineController.ts
@@ -1,4 +1,8 @@
+import { SettingsModel } from '../model/SettingsModel';
+
 export default interface IEngineController {
   connect(unitId: number): void;
   ping(unitId: number): void;
+  getSettings(): SettingsModel;
+  saveSettings(settings: SettingsModel): void;
 }
