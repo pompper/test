c9d7017abbaea673b23f344d0df4f03e7d560ae7
Born Scream <pompper@hotmail.com>
2024-01-16 13:49:38 +0700

    day 2 - new branch push

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,10 +1,65 @@
-import { Stack, Button } from "@mui/material"
+import { Stack, Button, IconButton, ButtonGroup, ToggleButtonGroup, ToggleButton } from "@mui/material"
+import SendIcon from '@mui/icons-material/Send';
+import FormatBold from "@mui/icons-material";
+import { FormatItalic } from "@mui/icons-material";
+import { FormatUnderlined } from "@mui/icons-material";
+// export const MuiButton = () => {
+//   return (
+//     <Stack spacing={2} direction='row' justifyContent='center'>
+//         <Button variant='contained'>Customer</Button>
+//         <Button variant='outlined'>Maintenance</Button>
+//     </Stack>
+//   )
+// }
+
 
 export const MuiButton = () => {
   return (
-    <Stack spacing={2} direction='row' justifyContent='center'>
-        <Button variant='contained'>Customer</Button>
-        <Button variant='outlined'>Maintenance</Button>
+    <Stack spacing={4}>
+      <Stack spacing={2} direction='row'>
+      <Button variant="text" href='https://www.google.com'>Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <Stack spacing={2} direction='row'>
+        <Button variant="contained" color="primary">Primary</Button>
+        <Button variant="contained" color="secondary">Secodary</Button>
+        <Button variant="contained" color="error">error</Button>
+        <Button variant="contained" color="warning">waring</Button>
+        <Button variant="contained" color="info">info</Button>
+        <Button variant="contained" color="success">success</Button>
+     </Stack>
+
+      <Stack display='block' spacing={2} direction='row'>
+        <Button variant='contained' size='small'>Small</Button>
+        <Button variant='contained' size='medium'>Medium</Button>
+        <Button variant='contained' size='large'>Small</Button>
+      </Stack>
+      <Stack spacing={2} direction='row'>
+        <Button variant='contained' startIcon={<SendIcon />} disableRipple onClick={() => alert('Click')}>Send</Button>
+        <Button variant='contained' endIcon={<SendIcon />} disableElevation>Send</Button>
+
+        <IconButton aria-label="send" color='success' size='small'>
+          <SendIcon />
+        </IconButton>
+      </Stack>
+
+      <Stack direction='row'>
+        <ButtonGroup 
+          variant="contained" 
+          orientation="vertical" 
+          size='small' 
+          color='secondary'
+          aria-label="alignment button group"
+          >
+          <Button onClick={() => alert('Left Clicked')}>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
     </Stack>
+
+     
   )
 }
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,20 +1,11 @@
 import { MemoryRouter as Router, Routes, Route, BrowserRouter, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import './App.css';
-import { Typography } from '@mui/material';
-import { Stack, Button } from "@mui/material"
 
-import { Box, Card, CardContent, Typography, Grid } from "@mui/material";
+import { MuiButton } from '../components/MuiButton';
 
-
-// function Hello() {
-//   const count = useAppSelector(selectCount);
-//   const dispatch = useAppDispatch();
-//   const [incrementAmount, setIncrementAmount] = useState('2');
-//     return (
-//     <div></div>
-//   );
-// }
+// Material UI
+import {Stack, Button, Box, Card, CardContent, Typography, Grid } from "@mui/material";
 
 // Main Page (Customer / Maintenance)
 function Main(){
@@ -67,12 +58,31 @@ function Monitor(){
 
   // Second API call (getSettings)
     window.electron.engineIpc.getSettings().then((data) => {
-      setSettingData(data);
+      // setSettingData(data);
+      // console.log(data)
+
+        // Store the data from the second API call in a separate variable
+      setSettingData({
+        plcConnections: data.plcConnections, // Replace with the actual structure of the data
+        plcReadConfig: data.plcReadConfig,
+        autoReconnectPLC: data.autoReconnectPLC,
+        stationInfo: data.stationInfo,
+        stationDataMap: data.stationDataMap,
+        baseBackendURL: data.baseBackendURL,
+        healthCheck: data.healthCheck,
+        password: data.password,
+        slots: data.stationDataMap.cabinets[0].slots
+      });
     });
   }, []); 
 
   const slots = stationLiveData?.cabinets[0]?.slots;
-  const hostAddress = settingData?.plcConnections[0]?.host;
+  
+  // Release Button 
+  const releaseButton = (slotId) => (e) => {
+    e.preventDefault();
+    console.log('The Release Button was clicked. > Slot id = ' + slotId);
+  }
 
   return (
     <div>
@@ -85,7 +95,7 @@ function Monitor(){
               <Box width='300px'>
                 <Card>
                     <CardContent>
-                        <Typography variant="subtitle1" component='div'>Cabinet 1: (Cabinet)</Typography>
+                        <Typography color='primary'><strong>Cabinet 1: (Cabinet)</strong> </Typography>
                         <Typography variant="subtitle1" component='div'>Status : Connected</Typography>
                     </CardContent>
                 </Card>
@@ -113,11 +123,87 @@ function Monitor(){
               </Grid>
             ) : (<p>No slots available</p>)}
 
-            {/*Station Setting Data */}
-            {/* Display host address here */}
-            {hostAddress && (
-              <Typography variant="subtitle1" component='div'>Host Address: {hostAddress}</Typography>
-            )}
+          {/*Station Setting Data */}
+            {settingData && (
+             <Box width='300px'>
+                <Card>
+                    <CardContent>
+                      <Typography color='primary'><strong>Cabinet 1</strong> </Typography>
+                      <Typography variant="subtitle1" component='div'>PLC IP: {settingData.plcConnections[0].host}</Typography>
+                      <Typography variant="subtitle1" component='div'>UnitID: {settingData.plcConnections[0].unitId}</Typography>
+                      <Typography variant="subtitle1" component='div'>Status: Connected</Typography>
+                    </CardContent>
+                </Card>
+              </Box>
+          )}
+
+          {/* Loop Slots */}
+          {settingData && (
+             <Grid container my={2} spacing={2}>    
+                {settingData.slots.map((slot, index) => (
+                  <Grid item xs={6} md={4} lg={3} key={index}>
+                    <Card>
+                      <CardContent>
+                        <Typography variant="subtitle1" component='div' >
+                          <Typography color='primary'><strong>Slot {index + 1}</strong> </Typography>
+                          <div>ID: {slot.id}</div>
+                          <div>Local ID: {slot.localId}</div>
+                          <Box my={2}>
+                            <Typography color='primary'><strong>RFID Config</strong> </Typography>
+                            <div>Type: {slot.rfidConfig.type}</div>
+                            <div>Start: {slot.rfidConfig.start}</div>
+                            <div>Length: {slot.rfidConfig.length}</div>
+                          </Box>      
+                          <Button variant='contained' onClick={releaseButton(slot.id)}>Release</Button>
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+          )}            
+
+          {/* Server Health */}
+          {settingData && (
+            <Box my={2}>
+                <Card>
+                    <CardContent>
+                      <Typography color='primary'><strong>Server Health</strong> </Typography>
+                      <Typography variant="subtitle1" component='div'>Status: Connected</Typography>
+                    </CardContent>
+                </Card>
+              </Box>
+          )}
+
+          {/* Station Info */}
+          {settingData && (
+            <Box my={2}>
+                <Card>
+                    <CardContent>
+                      <Typography color='primary'><strong>Station Info</strong> </Typography>
+                      <Typography variant="subtitle1" component='div'>Station ID: {settingData.stationInfo.id}</Typography>
+                      <Typography variant="subtitle1" component='div'>Address: {settingData.stationInfo.address}</Typography>
+                      <Typography variant="subtitle1" component='div'>Code: {settingData.stationInfo.code}</Typography>
+                      <Typography variant="subtitle1" component='div'>Type: {settingData.stationInfo.type}</Typography>
+                      <Typography variant="subtitle1" component='div'>Version: {settingData.stationInfo.version}</Typography>
+                      <Typography variant="subtitle1" component='div'>Last Updated: {settingData.stationInfo.lastUpdateTimestamp}</Typography>
+                    </CardContent>
+                </Card>
+              </Box>
+          )}
+
+           {/* Additional Info */}
+          {settingData && (
+            <Box my={2}>
+                <Card>
+                    <CardContent>
+                      <Typography color='primary'><strong>Addition Info</strong> </Typography>
+                      <Typography variant="subtitle1" component='div'>Auto Reconnect PLC: {settingData.autoReconnectPLC ? "true" : "false"}</Typography>
+                      <Typography variant="subtitle1" component='div'>Base Backend URL: {settingData.baseBackendURL}</Typography>
+                    </CardContent>
+                </Card>
+              </Box>
+          )}
           </section>
       </Stack>
       <Link to="/"><Button variant='text'>Back</Button></Link>
@@ -148,12 +234,14 @@ function Sandbox(){
       <MaintainenceMenu page="sandbox"/>
      <section>
       <Typography variant='h4'>Sandbox</Typography>
+      <MuiButton/>
      </section>
     </Stack>
     <Link to="/"><Button variant='text'>Back</Button></Link>
     </div>
   )
 }
+
 
 export default function App() {
   return (
